01a698bf1d2cddf1bfbb922057fcd97eda4ddb2f
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-07 15:33:42 +0100

    init commit - state vars work

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { mainTitle } from '../stores';
+
+	let count: number = 0;
+	let title: string = 'initial title';
+	const increment = () => {
+		count += 1;
+	};
+
+	const reset = () => {
+		count = 0;
+	};
+
+	$: {
+		if (count > 10) {
+			title = 'too much! (title)';
+
+			mainTitle.update((n) => n + 'too much! (main title)');
+		}
+	}
+</script>
+
+<h1>{title}</h1>
+<button on:click={increment}>
+	count is {count}
+</button>
+<button on:click={reset}>Reset</button>
+
+<style>
+	h1 {
+		color: green;
+	}
+</style>
---
+++ src/stores.js
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+
+export const mainTitle = writable('init main from store');
